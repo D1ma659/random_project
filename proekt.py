5e98cf3cf342ef84e3cc1da05598d322087646d9
D1ma659 <gameriston777@gmail.com>
2026-08-18 14:57:24 +0300

    Update proekt.py

--- proekt.py
+++ proekt.py
@@ -1,8 +1,22 @@
 from random import *
 print("Привет! ТЫ попал в игру угадай число")
-print("Тебе нужно угадать число от 1 до 100 УДачи")
+print("Тебе нужно угадать число от 1 до 100 Удачи")
 n = randint(1,100)
-attemps=7
+print("Выбери сложность: ")
+print("---1. Легко(10 попыток)")
+print("---2. Легко(7 попыток)")
+print("---3. Легко(4 попытки)")
+user = int(input("Вебри сложность 1-2-3 "))
+if user == 1:
+    attemps=10
+elif user ==2:
+    attemps=7
+elif user ==3:
+    attemps=4
+else:
+    print("Такой сложности нет!!!!!")
+    print("ТОГДА ТЫ БУДЕШЬ ПРОХОДИТЬ ХАРДКОРД")
+    attemps=2
 while attemps > 0:
     user=int(input("Введи число: "))
     attemps = attemps - 1
